123930850651bf02fff4bd44ff73130e3a9d0b80
Chocholouš Pavel <chocholousp@avast.com>
2019-08-20 09:45:46 +0200

    code highlight a upravy tahaku

--- src/components/Tahak.vue
+++ src/components/Tahak.vue
@@ -3,7 +3,65 @@
     <v-layout>
         <v-flex>
            <h1>Bude pripraveno Monikou Vlckovou :)</h1>
+
+
+            <v-card>
+            	<v-tabs>
+              	<v-tab>Barevnej select</v-tab>
+              	<v-tab>Tildy a chlupatý závorky</v-tab>
+
+              	<v-tab-item>
+                 <v-card flat color="basil">
+                  	<v-card-text>
+                      Psaní selektů podléhá nekompromisním pravidlům. Jinak řečeno, záleží na pořadí klíčových slov. Následující "šablona" ukazuje tato pravidla na příkladu. 
+                    </v-card-text>
+                    <v-card-text>
+                    	<pre v-highlightjs><code class="sql">
+-- všechno, co je za dvěma pomlčkami až do konce řádku je komentář, tedy to engine ignoruje
+/*
+víceřádkové komentáře vypadají
+takhle
+*/
+SELECT	--příkazem SELECT enginu říkáme, že chceme aby nám ukázal/vypsal data
+nas_sloupecek1, nas_sloupecek2, nas_sloupecek3 AS datum, 
+	/* vyjmenováním sloupečků říkáme, které chceme vidět, oddělujeme je 		
+	čárkami, můžeme taky použít alias (AS) */
+funkce(nas_sloupecek1) AS s1	--použitím funkcí můžeme zobrazené sloupečky měnit, kombinovat apod.
+FROM	-- když chceme “selectovat” musíme enginu říct odkud
+nase_tabulka
+	/* nejjednodušší je výběr dat z jedné tabulky, za klíčovým slovem 	
+	FROM můžeme ale i joinovat tabulky, nebo používat vnořené selecty */
+WHERE	/* za WHERE patří podmínky (filtry) určující, které řádky engine vybere, 
+	i 	tady můžeme použít funkce a kombinovat sloupečky */
+((nas_sloupecek1 = 'textova hodnota')
+OR
+(nas_sloupecek2 = 11))
+AND
+funkce(nas_sloupecek3) > '2001-09-11'
+GROUP BY 
+	/* pokud chceme použít agregační funkce musíme enginu říct podle kterých
+	sloupečků má data agregovat */
+HAVING	/*filtrje skupinky poDLE AGREGACNICH FUNKCI, TREBA NULOVY SOUCET POCTU 
+	MRTVYCH, CENY APOD.*/
+nas_sloupecek1, nas_sloupecek2, nas_sloupecek3
+ORDER BY 	/* pokud chceme data řadit musíme enginu říct podle kterých sloupečků */
+nas_sloupecek1, nas_sloupecek2, nas_sloupecek3
+LIMIT 11	-- nakonec můžeme omezit počet řádek, které chceme zobrazit 
+
+                    	</code></pre>
+					</v-card-text>
+                 </v-card>
+              	 </v-tab-item>
+
+              	 <v-tab-item>
+                 <v-card flat color="basil">
+                  <v-card-text>Středníky a spol</v-card-text>
+                 </v-card>
+              	 </v-tab-item>
+              	</v-tabs>
+            </v-card>
         </v-flex>
+
     </v-layout>
   </v-container>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,21 @@ import theme from './theme'
 import Vuetify from "vuetify"
 import App from './App.vue'
 import router from "./router"
-
+//code highlighting (sql)
+import VueHighlightJS from "vue-highlightjs"
+import sql from "highlight.js/lib/languages/sql"
+require('../node_modules/highlight.js/styles/ir-black.css')
 Vue.config.productionTip = false
+
+/*
+* Use Vue Highlight.js
+*/
+Vue.use(VueHighlightJS, {
+	// Register only languages that you want
+	languages: {
+		sql
+	}
+});
 
 Vue.use(Vuetify, {
   theme
