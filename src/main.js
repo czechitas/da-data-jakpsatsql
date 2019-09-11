6ea7b1a25b1e5a8edef01c33c511572b1e94f268
Chocholouš Pavel <chocholousp@avast.com>
2019-09-11 13:54:05 +0200

    css

--- src/main.js
+++ src/main.js
@@ -1,36 +1,33 @@
-import Vue from 'vue'
-import Viewer from 'v-viewer'
-import './plugins/vuetify'
-import theme from './theme'
-import Vuetify from "vuetify"
-import App from './App.vue'
-import router from "./router"
+import Vue from "vue";
+import Viewer from "v-viewer";
+import "./plugins/vuetify";
+import theme from "./theme";
+import Vuetify from "vuetify";
+import App from "./App.vue";
+import router from "./router";
 //code highlighting (sql)
-import VueHighlightJS from "vue-highlightjs"
-import sql from "highlight.js/lib/languages/sql"
-require('../node_modules/highlight.js/styles/ir-black.css')
-Vue.config.productionTip = false
+import VueHighlightJS from "vue-highlightjs";
+import sql from "highlight.js/lib/languages/sql";
+require("../node_modules/highlight.js/styles/atelier-savanna-light.css");
+Vue.config.productionTip = false;
 
 /*
-* Use Vue Highlight.js
-*/
+ * Use Vue Highlight.js
+ */
 Vue.use(VueHighlightJS, {
-	// Register only languages that you want
-	languages: {
-		sql
-	}
+  // Register only languages that you want
+  languages: {
+    sql
+  }
 });
 
 Vue.use(Vuetify, {
   theme
-})
+});
 
-Vue.use(Viewer)
+Vue.use(Viewer);
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
-
-
-
+  render: h => h(App)
+}).$mount("#app");
