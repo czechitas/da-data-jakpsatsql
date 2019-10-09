f57bdb7a7f6de64b70aa9470f56fcfae3915582c
Ondřej Hošák <ondrej.hosak@avast.com>
2019-10-09 18:50:30 +0200

    pridani vue config

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+var webpack = require("webpack")
+
+module.exports = {
+  configureWebpack: {
+    plugins: [new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /(en|cs)$/)],
+    entry: "./src/main.js",
+    module: {
+        rules: [
+            {
+                test: /\.(csv)$/,
+                use: [{ loader: "file-loader", options: { name: "static/[name].csv"}}],
+            },
+        ]
+    }
+  }
+}
